9c85ef8cac42285d1c2f918a42973d483740dfe4
johandev21 <johan.p.2160@gmail.com>
2025-03-09 21:29:20 -0300

    feat(sidebar): implement AppSidebar with navigation and user profile components
    - Added AppSidebar component with collapsible sidebar functionality.
    - Implemented NavMain for main navigation links (Home, Notifications, Saved, Profile).
    - Added NavUser for user profile management with avatar, dropdown menu, and logout functionality.
    - Integrated responsive behavior and theme toggle support.

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,66 @@
+import * as React from "react"
+import {
+    Bell,
+  Bookmark,
+  Home,
+  User,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+import { RiTwitterLine } from "@remixicon/react"
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Home",
+      url: "#",
+      icon: Home,
+      isActive: true,
+    },
+    {
+      title: "Notifications",
+      url: "#",
+      icon: Bell,
+    },
+    {
+      title: "Saved",
+      url: "#",
+      icon: Bookmark,
+    },
+    {
+      title: "Profile",
+      url: "#",
+      icon: User,
+    }
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <RiTwitterLine size={32} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
