8b6d4b57710d4f11730b51237e726be3526cd1b4
johandev21 <johan.p.2160@gmail.com>
2025-03-11 14:35:33 -0300

    feat(NotificationsPage): add component for displaying user notifications
    - Added a NotificationsPage component to display user notifications, such as likes and follows.
    - Included details like the user who performed the action, the content of the post (if applicable), and the time since the notification occurred.
    - Designed with a responsive layout and used Card components for consistent styling.
    - Integrated Lucide icons (Heart and UserPlus) for visual representation of notification types.

---
+++ src/pages/NotificationsPage.tsx
@@ -0,0 +1,43 @@
+import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
+import { Heart, UserPlus } from "lucide-react";
+
+export default function NotificationsPage() {
+  return (
+    <div className="flex justify-center h-full">
+      <div className="w-full max-w-2xl">
+        <div className="flex justify-between p-4">
+          <h1 className="font-medium">Notifications</h1>
+          <p>0 unread</p>
+        </div>
+        {/* <NotificationCard /> */}
+        <div className="flex flex-col p-4 gap-4">
+          <Card className="w-full">
+              <CardHeader className="flex-row items-center">
+                <div className="size-8 bg-muted rounded-full" />
+                <CardTitle className="font-medium">@shadowelkaiser liked your post</CardTitle>
+                <Heart width={16} />
+              </CardHeader>
+              <CardContent>
+                <div className="bg-muted rounded-sm p-2">
+                  <p>Happy new year everyone! 🎆🥳</p>
+                </div>
+              </CardContent>
+              <CardFooter>
+                <p className="text-xs">16 hours ago</p>
+              </CardFooter>
+          </Card>
+          <Card className="w-full">
+              <CardHeader className="flex-row items-center">
+                <div className="size-8 bg-muted rounded-full" />
+                <CardTitle className="font-medium">@shadowelkaiser started following you</CardTitle>
+                <UserPlus width={16} />
+              </CardHeader>
+              <CardFooter>
+                <p className="text-xs">16 hours ago</p>
+              </CardFooter>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
