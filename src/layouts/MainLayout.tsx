e05c0226fcfa95d0d2d7348f641d10841b7f4273
johandev21 <johan.p.2160@gmail.com>
2025-03-20 18:07:22 -0300

    style(MainLayout): enhance header with sticky positioning and backdrop blur
    - Added sticky positioning to the header to keep it fixed at the top of the viewport.
    - Applied ackdrop-blur-[0.2rem] to the header for a subtle blur effect, improving visual appeal.
    - Added 	op-0 and z-5 to ensure the header stays at the top of the layout and above other content.
    - Maintained existing functionality and structure of the layout.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -11,7 +11,7 @@ export function MainLayout() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <header className="sticky backdrop-blur-[0.2rem] top-0 z-5 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
           </div>
